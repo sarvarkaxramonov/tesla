517ce7abd944e7bcce3df38bb973671fcee6a4e3
sarvar kaxramonov <162875518+sarvarkaxramonov@users.noreply.github.com>
2024-03-10 15:14:45 +0500

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,21 @@
+// let currentIndex = 0;
+// const slides = document.querySelectorAll('.slide');
+//
+// function showSlide(index) {
+//     slides.forEach((slide, i) => {
+//         slide.style.display = i === index ? 'block' : 'none';
+//     });
+// }
+//
+// function prevSlide() {
+//     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+//     showSlide(currentIndex);
+// }
+//
+// function nextSlide() {
+//     currentIndex = (currentIndex + 1) % slides.length;
+//     showSlide(currentIndex);
+// }
+//
+// // Show the first slide initially
+// showSlide(currentIndex);
